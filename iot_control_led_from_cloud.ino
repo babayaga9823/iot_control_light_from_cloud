957d1a21bdce2d2ac0e5f868dcdd401bf12ea509
babayaga9823 <babayaga9823@gmail.com>
2019-10-20 21:19:37 +0530

    branch updated

--- iot_control_led_from_cloud.ino
+++ iot_control_led_from_cloud.ino
@@ -6,7 +6,7 @@
 #define WIFI_SSID "You wifi name to which it is to be connected"                                          // input your home or public wifi name 
 #define WIFI_PASSWORD "password of your wifi"                                    //password of wifi ssid
 
-String fireStatus = "";                                                     // led status received from firebase
+String fireStatus = "";                                                   
 String mycmd = "";  
 int led = D3; 
 int ledd = D8; // for external led
@@ -28,33 +28,32 @@ void setup() {
   Serial.println(WIFI_SSID);
   Serial.print("IP Address is : ");
   Serial.println(WiFi.localIP());                                                      //print local IP address
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                                       // connect to firebase
-  Firebase.setString("LED_STATUS", "OFF");                                          //send initial string of led status
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                                       
+  Firebase.setString("LED_STATUS", "OFF");                                         
    Firebase.setString("MY_personal_led_status", "OFF"); 
 }
 
 void loop() {
   fireStatus = Firebase.getString("LED_STATUS");       
   mycmd = Firebase.getString("MY_personal_led_status");   // get ld status input from firebase
-  if (fireStatus == "ON"){                                                          // compare the input of led status received from firebase
+  if (fireStatus == "ON"){                                                        
     Serial.println("Led Turned ON");                         
-    digitalWrite(LED_BUILTIN, LOW);                                                  // make bultin led ON
-    digitalWrite(led, HIGH);                                                         // make external led ON
+    digitalWrite(LED_BUILTIN, LOW);                                                 
+    digitalWrite(led, HIGH);                                                        
   } 
-   if (mycmd == "ON"){                                                          // compare the input of led status received from firebase
+   if (mycmd == "ON"){                                                          
     Serial.println("My personal Led Turned ON");                         
-    digitalWrite(LED_BUILTIN, LOW);                                                  // make bultin led ON
-    digitalWrite(ledd, HIGH);                                                         // make external led ON
-  } 
-    if (mycmd == "OFF"){                                                  // compare the input of led status received from firebase
+    digitalWrite(LED_BUILTIN, LOW);                                                 
+    digitalWrite(ledd, HIGH);                                                        
+    if (mycmd == "OFF"){                                                  
     Serial.println("My personal Led Turned OFF");
-    digitalWrite(LED_BUILTIN, HIGH);                                               // make bultin led OFF
-    digitalWrite(ledd, LOW);                                                         // make external led OFF
+    digitalWrite(LED_BUILTIN, HIGH);                                             
+    digitalWrite(ledd, LOW);                                                         
   }
-   if (fireStatus == "OFF") {                                                  // compare the input of led status received from firebase
+   if (fireStatus == "OFF") {                                                 
     Serial.println("Led Turned OFF");
-    digitalWrite(LED_BUILTIN, HIGH);                                               // make bultin led OFF
-    digitalWrite(led, LOW);                                                         // make external led OFF
+    digitalWrite(LED_BUILTIN, HIGH);                                               
+    digitalWrite(led, LOW);                                                      
   }
   else {
     Serial.println("Wrong Credential! Please send ON/OFF");
